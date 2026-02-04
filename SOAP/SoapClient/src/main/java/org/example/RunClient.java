0b7470c6116dc3f69ce3c66f5de3607fe4c7ef79
dangthuydungt67-boop <dangthuydung_t67@hus.edu.vn>
2026-02-04 15:24:22 +0700

    RunClient.java

--- SOAP/SoapClient/src/main/java/org/example/RunClient.java
+++ SOAP/SoapClient/src/main/java/org/example/RunClient.java
@@ -7,29 +7,21 @@ import java.util.Scanner;
 public class RunClient {
     public static void main(String[] args) {
         try {
-            // Khởi tạo Service và Port (Cầu nối)
             UserWebServiceService service = new UserWebServiceService();
             UserWebService port = service.getUserWebServicePort();
 
-            // Nhập liệu từ bàn phím
             Scanner scanner = new Scanner(System.in);
-            System.out.println("=== CHƯƠNG TRÌNH SOAP CLIENT ===");
             System.out.print("Nhập tên của bạn: ");
             String name = scanner.nextLine();
             System.out.print("Nhập số điện thoại: ");
             String phone = scanner.nextLine();
-
-            // Gọi hàm lên Server
-            System.out.println("Đang gửi dữ liệu...");
             String response = port.guiThongTin(name, phone);
 
-            // In kết quả trả về
-            System.out.println("------------------------------");
             System.out.println("Kết quả từ Server: " + response);
-            System.out.println("------------------------------");
 
         } catch (Exception e) {
             System.err.println("Lỗi khi kết nối Server: " + e.getMessage());
         }
     }
+
 }
